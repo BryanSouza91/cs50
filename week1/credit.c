fa2f9432b2a60951456f0f34d4c248bbaee62138
Bryan Souza <brybryflys@gmail.com>
2023-01-29 12:46:31 -0800

    added comments to half.c and credit.c;

--- week1/credit.c
+++ week1/credit.c
@@ -1,18 +1,25 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// helper function
 #define between(x, a, b)  (((a) <= (x)) && ((x) <= (b)))
 
+// global declaration to pass through functions
 int first2;
 
+// function declaration to validate CC Number Checksum
 bool valid(long card_number);
+// function declaration to identify CC provider
 string identify_provider(int first2);
 
 int main(void) 
 {
+    // request CC Number from user
     long cc_num = get_long("CC Number:\t");
+    // validate CC Number
     if (valid(cc_num))
     {
+        // if valid, identify provider
         printf("%s\n", identify_provider(first2));
     }
     else
@@ -21,16 +28,20 @@ int main(void)
     } 
 }
 
+// Validates CC Number Checksum using Luhn's Algorithm
 bool valid(long card_number)
 {
     int c = 1, sum_a = 0, sum_b = 0, total = 0;
     do
     {
         // printf("Counter:\t%i\n", c);
+        // iterate through cc number in reverse
         int digit = card_number % 10;
+        // counter to keep track of iterations
         if (c % 2 == 0)
         {
             // printf("Digit:\t%i\n", digit);
+            // adding the digits of the products to sum a
             if (digit * 2 >= 10)
             {
                 sum_a += (digit * 2 - 10);
@@ -43,11 +54,14 @@ bool valid(long card_number)
                 // printf("Sum_a:\t%i\n", sum_a);
             }
         }
+        // adding the other digits to sum b
         else
         {
             sum_b += digit;
         }
+        // increment the counter
         c++;
+        // capture the first 2 digits of the card to pass to provider identification function
         if (between(card_number, 10, 99))
         {
             first2 = card_number;
@@ -55,6 +69,7 @@ bool valid(long card_number)
         }
     }
     while (card_number /= 10);
+    // add sums of digits to validate checksum
     total = sum_a + sum_b;
     // printf("Total:\t%i\nLast Digit:\t%i\n", total, total % 10);
     if (total % 10 == 0)
@@ -64,12 +79,15 @@ bool valid(long card_number)
     return false;
 }
 
+// Identify CC Provider by checking the first 2 digits of the CC Number
 string identify_provider(int first2)
 {
+    // Define various providers
     string a = "AMEX";
     string v = "VISA";
     string m = "MASTERCARD";
     string d = "DISCOVER";
+    // determine provider and return name
     switch (first2)
     {
         case 34:
--- week1/half.c
+++ week1/half.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <cs50.h>
 
+// function declaration 
 float half(float bill, float tax, float tip);
 
 int main(void)
 {
+    // request information from the user
     float subtotal = get_float("Bill before tax and tip:\t");
     float tax_rate = get_float("Sales Tax Rate:\t");
     float tip_rate = get_float("Tip Rate:\t");
 
-    float total = half(subtotal, tax_rate, tip_rate);
-
-    printf("You each pay $%.2f!\n", total);
+    // return half of bill to user
+    printf("You each pay $%.2f!\n", half(subtotal, tax_rate, tip_rate));
 }
 
+// calculate tax add to bill
+// calculate tip from bill w/tax and add to bill
+// return half of bill 
 float half(float bill, float tax, float tip)
 {
     tax /= 100;

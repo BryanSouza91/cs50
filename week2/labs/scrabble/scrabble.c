42c580bab403ec4be888e59dc55b04b94651b70d
Bryan Souza <brybryflys@gmail.com>
2023-01-31 09:38:14 -0800

    finished week2 labs;

---
+++ week2/labs/scrabble/scrabble.c
@@ -0,0 +1,55 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word);
+
+int main(void)
+{
+    // Get input words from both players
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Score both words
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else if (score1 == score2)
+    {
+        printf("Tie!\n");
+    }
+}
+
+int compute_score(string word)
+{
+    // TODO: Compute and return score for string
+    // get length of word and set score to 0
+    int length = strlen(word), score = 0;
+    for (int i = 0; i < length; i++)
+    {
+        // change letter to uppercase
+        word[i] = toupper(word[i]);
+        // typecast letter to int to get ASCII index
+        int index = (int) word[i];
+        // adjust index to match POINTS index
+        index -= 65;
+        // calculate score
+        score += POINTS[index];
+    }
+    // return total score by word
+    return score;
+}
